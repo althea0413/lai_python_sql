11428baf8507fd41d753f3c9ee8cc9ec4563f685
althea <susu041305@gmail.com>
2024-10-06 16:05:00 +0800

    第三次上課

---
+++ sql/lesson3_4.sql
@@ -0,0 +1,30 @@
+CREATE TABLE student(
+	student_id SERIAL,
+	name VARCHAR(20),
+	major VARCHAR(20),
+	PRIMARY KEY(student_id)
+);
+
+SELECT *
+FROM student;
+
+/*新增資料*/
+INSERT INTO student(name,major)
+VALUES ('小白','歷史');
+
+INSERT INTO student(major,name)
+VALUES ('生物','小黑');
+
+INSERT INTO student(major,name)
+VALUES (NULL,'小藍');
+
+/*新增多筆資料*/
+INSERT INTO student(major,name)
+VALUES ('國文','小線'),('數學','小綠');
+
+/*傳回新增的資料*/
+INSERT INTO student(major,name)
+VALUES ('生物','小線'),('歷史','小綠')
+RETURNING*;
+
+DROP TABLE IF EXISTS student;
---
+++ sql/lesson3_5.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS student(
+	student_id SERIAL,
+	name VARCHAR(20),
+	major VARCHAR(20),
+	score INT,
+	PRIMARY KEY (student_id)
+);
+
+INSERT INTO student VALUES(1,'小白','英語','50');
+INSERT INTO student VALUES(2,'小黃','生物','90');
+INSERT INTO student VALUES(3,'小綠','歷史','70');
+INSERT INTO student VALUES(4,'小藍','英語','80');
+INSERT INTO student VALUES(5,'小黑','化學','20');
+
+INSERT INTO student
+VALUES (1, '小白','英語',50),
+	   (2, '小黃','生物',90),
+	   (3, '小綠','歷史',70),
+	   (4, '小藍','英語',80),
+	   (5, '小黑','化學',20)
+RETURNING *;
+
+SELECT *
+FROM student;
+
+DROP TABLE IF EXISTS student;
+
+DELETE FROM student; /*資料全部刪除*/
+
+DELETE FROM student
+WHERE name = '小白';
+
+DELETE FROM student
+WHERE name LIKE '小%';
+
+/*修改欄位中的名稱*/
+UPDATE student
+SET major = '英語文學'
+WHERE  major = '英語'
+RETURNING *;
+
+UPDATE student
+SET major = '生化'
+WHERE  major = '生物' OR major ='化學'
+RETURNING *;
+
+
