df2d6d5e83a66d97285ff98a509959882625645c
althea <susu041305@gmail.com>
2024-10-06 12:02:33 +0800

    第三堂上課

--- sql/lesson3_2.sql
+++ sql/lesson3_2.sql
@@ -1,2 +1,2 @@
-SELECT code,name
+SELECT code as 代碼,name as 車站名稱,e_name as 英文名稱
 FROM stations;
---
+++ sql/lesson3_3.sql
@@ -0,0 +1,24 @@
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE code = '1001'or code='1002'
+
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE code IN('1001','1002');
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台_';
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台%';
+ 
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE id <=10;
+
+SELECT id as 序號,code as 代碼,name as 車站名稱,e_name as 英文名稱
+FROM stations
+WHERE id >=10 AND id<=20;
