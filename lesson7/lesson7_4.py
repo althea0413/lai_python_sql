d3ef10738f551b2801692a6ae13d617b1aa11b70
althea <susu041305@gmail.com>
2024-11-03 15:54:08 +0800

    modify

---
+++ lesson7/lesson7_4.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+conn = psycopg2.connect(host=os.environ['HOST'],database=os.environ['DATABASE'],user=os.environ['USER'],password=os.environ['PASSWORD'])
+with conn:
+    with conn.cursor() as cursor:
+        sql = '''
+        SELECT * FROM 市場
+        '''
+        cursor.execute(sql)
+        all_country = cursor.fetchall()
+conn.close()
+
+st.title('世界大盤分析')
+with st.sidebar:
+    st.title('請選擇股票市場:')
+    st.write('## 台灣')
+    st.write(all_country)
