2625c0f30277cc46920cde30eab42d50cf0af16d
lai <susu041305@gmail.com>
2024-11-10 15:50:17 +0800

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -2,11 +2,34 @@ import streamlit as st
 import psycopg2
 from dotenv import load_dotenv
 import os
+import pandas as pd
 load_dotenv()
+
+@st.cache_data
+def getData(country:tuple[str])->list[tuple]:
+    conn = psycopg2.connect(host=os.environ['HOST'],
+                            database=os.environ['DATABASE'],
+                            user=os.environ['USER'],
+                            password=os.environ['PASSWORD'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql = '''
+            SELECT country,市場.name,date,adj_close,volume
+            FROM 股市 JOIN 市場 ON 股市.name = 市場.name
+            WHERE country IN %s;
+            '''
+            cursor.execute(sql,(country,))
+            all_data = cursor.fetchall()
+    conn.close()
+    return all_data
+
 
 @st.cache_resource
 def get_country():
-    conn = psycopg2.connect(host=os.environ['HOST'],database=os.environ['DATABASE'],user=os.environ['USER'],password=os.environ['PASSWORD'])
+    conn = psycopg2.connect(host=os.environ['HOST'],
+                            database=os.environ['DATABASE'],
+                            user=os.environ['USER'],
+                            password=os.environ['PASSWORD'])
     with conn:
         with conn.cursor() as cursor:
             sql = '''
@@ -17,22 +40,23 @@ def get_country():
     conn.close()
     input_dict = dict(all_country)
     return input_dict.values()
-
-def user_select():
-    print('使用者選擇了')
-    print(st.session_state.stocks)
-
-
-st.title('世界大盤分析')
-with st.sidebar:
-    default_country = '台灣'
-    st.title('請選擇股票市場:')
+   
     
-    st.multiselect("請選擇",get_country(),
-                    default=default_country,
-                    placeholder="請選擇市場",
-                    label_visibility='hidden',
-                    key='stocks',
-                    on_change=user_select)
-    st.write(default_country)
+
+
+default_country = '台灣'
+with st.sidebar:    
+    st.title('請選擇股票市場:')    
+    options:list[str] =st.multiselect("請選擇",get_country(),
+                        default=default_country,
+                        placeholder="請選擇市場",
+                        label_visibility='hidden',
+                        key='stocks'
+                        )
     
+selectedCountry:tuple[str] = tuple(options) 
+if selectedCountry: 
+    st.title('世界大盤分析')
+    df = pd.DataFrame(getData(selectedCountry),columns=['國家','代號','日期','收盤價','成交量'])
+    df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
+    st.line_chart(data=df,x='日期',y='收盤價',color='國家')
