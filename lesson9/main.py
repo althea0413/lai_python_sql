a19012c0f43e0100ae8bb2545cf3051f2a60571e
althea <susu041305@gmail.com>
2024-11-17 10:48:55 +0800

    modify

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,17 +1,20 @@
-
 from dotenv import load_dotenv
 import streamlit as st
 from time import sleep
-
+from datasource import get_data
 load_dotenv()
-placeholder = st.empty()
-with placeholder:
-    with st.spinner('下載資料中...'):
-        sleep(5)
 
-    st.success("下載完成",icon=":material/favorite:")
-    sleep(3)
+
+@st.dialog("目前發生問題,請稍後再試")
+def alert():
+        st.write('連線有問題')
+        st.stop()
+
+with st.spinner('下載資料中...'):
+        get_data()
+alert()
+st.write('顯示頁面')
+
 
 
 placeholder.empty()
-    
