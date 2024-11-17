090c8836912e14918f8eb08c94fc1201092a0e1c
althea <susu041305@gmail.com>
2024-11-17 14:16:08 +0800

    modify

--- lesson9/tools/__datasource__.py
+++ lesson9/tools/__datasource__.py
@@ -1,3 +1,6 @@
+import requests
+
+
 #常數
 PI = 3.1415926
 
@@ -7,6 +10,13 @@ class CustomClass:
 
 import time
 #自訂的function
-def get_data():
-    time.sleep(5)
+def get_data()->list | None:
     url='https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except:
+        print("出錯了")
+        return None
+    else:
+        return response.json()
